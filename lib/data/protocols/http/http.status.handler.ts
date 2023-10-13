0f5fbc22afbbd6335ecc0d2ad73177ab32eb0c21
Gilberto Santos <gsantos@g-p.com>
2023-10-13 19:13:25 +0100

    code:fix

--- lib/data/protocols/http/http.status.handler.ts
+++ lib/data/protocols/http/http.status.handler.ts
@@ -1,27 +1,33 @@
-import { BadGatewayError, BadRequestError, ForbiddenError, InvalidCredentialError, NotFoundError } from '@/domain';
-import { HttpResponse } from './http-response';
-import { HttpStatusCode } from './http.status';
+import {
+  BadGatewayError,
+  BadRequestError,
+  ForbiddenError,
+  InvalidCredentialError,
+  NotFoundError
+} from '@/domain'
+import { HttpResponse } from './http-response'
+import { HttpStatusCode } from './http.status'
 
 export abstract class HttpStatusHandler {
   protected whichHttpResponse(res: HttpResponse) {
     switch (res.code) {
       case HttpStatusCode.ok:
-        return res.body;
+        return res.body
 
       case HttpStatusCode.badRequest:
-        throw new BadRequestError(res.body);
+        throw new BadRequestError(res.body)
 
       case HttpStatusCode.notFound:
-        throw new NotFoundError();
+        throw new NotFoundError()
 
       case HttpStatusCode.badGateway:
-        throw new BadGatewayError();
+        throw new BadGatewayError()
 
       case HttpStatusCode.forbidden:
-        throw new ForbiddenError();
+        throw new ForbiddenError()
 
       case HttpStatusCode.unauthorized:
-        throw new InvalidCredentialError();
+        throw new InvalidCredentialError()
     }
   }
 }
